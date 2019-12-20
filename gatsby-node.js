332139cad04dc402d4bb0d3f73d0916844a395c8
Jeff Heilbut <jeff.heilbut@cen.centeva.com>
2019-12-20 11:59:51 -0700

    initial push

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,34 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+/* gatsby-node.js */
 
-// You can delete this file if you're not using it
+const path = require(`path`)
+
+exports.createPages = async ({ actions, graphql, reporter }) => {
+  const { createPage } = actions
+  const template = path.resolve(`src/templates/blogTemplate.js`)
+
+  const result = await graphql(`
+    {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              path
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  if (result.errors) {
+    reporter.panicOnBuild(`Error fetching blogs`)
+    return
+  }
+
+  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    createPage({
+      path: node.frontmatter.path,
+      component: template,
+    })
+  })
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,59 @@
 import React from "react"
-import { Link } from "gatsby"
+import { graphql, Link } from "gatsby"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+import style from "./index.module.scss"
+import Img from 'gatsby-image'
+
+const IndexPage = ({ data }) => {
+  console.log(data)
+  return (
+    <div className={style.home}>
+      <SEO title="Centeva Blog" />
+      <h1>Centeva Blog</h1>
+
+      {data.allMarkdownRemark.edges.map(({ node }) => (
+        <div key={node.id}>
+          <h2>
+            <Link to={node.frontmatter.path}>{node.frontmatter.title}</Link>
+          </h2>
+          <div className={style.image}>
+            <Img fluid={node.frontmatter.avatar.childImageSharp.fluid} />
+          </div>
+          <p>{node.frontmatter.author}</p>
+          <p>{node.excerpt}</p>
+        </div>
+      ))}
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+  )
+}
 
 export default IndexPage
+
+export const query = graphql`
+{
+  allMarkdownRemark {
+    edges {
+      node {
+        excerpt
+        frontmatter {
+          author
+          path
+          title
+          avatar {
+            childImageSharp {
+              fluid(maxWidth: 250) {
+                aspectRatio
+                sizes
+                src
+                srcSet
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+`
